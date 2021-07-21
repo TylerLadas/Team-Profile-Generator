04f489442918fcf6ef818ff2e1f389727dc6ee11
TylerLadas <tylerladas@hotmail.com>
2021-07-21 15:28:57 -0400

    create index.html template

---
+++ src/template.js
@@ -0,0 +1,44 @@
+//function to create employee card
+let generateCard = data => {
+    return `
+    <div class="col-3"
+        <div class="card" style="width: 18rem;">
+        <div class="card-header">
+            Featured
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+        </ul>
+        </div>
+    </div>
+    `;
+}
+
+//function to generate html for dist/index.html
+let generateHTML = data => {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>My Team</title>
+</head>
+<body>
+    <header>
+        <h1>My Team</h1>
+    </header>
+    <div class="row">
+
+    </div>
+</body>
+</html>
+`;
+};
+
+module.exports = generateHTML;
