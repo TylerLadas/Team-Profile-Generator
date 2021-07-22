e501313934a03055410b0e89dd5fdde720e8ffb6
TylerLadas <tylerladas@hotmail.com>
2021-07-22 12:56:51 -0400

    update template

--- src/template.js
+++ src/template.js
@@ -4,12 +4,13 @@ let generateManagerCard = manager => {
     <div class="col-3"
         <div class="card" style="width: 18rem;">
         <div class="card-header">
-            {{ name }}
+            <h3>${manager.name}</h3>
+            <h4>Manager</h4>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: {{ id }}</li>
-            <li class="list-group-item">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
-            <li class="list-group-item">Office Number: {{ office }}</li>
+            <li class="list-group-item">ID: ${manager.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+            <li class="list-group-item">Office Number: ${manager.office}</li>
         </ul>
         </div>
     </div>
@@ -22,12 +23,13 @@ let generateEngineerCard = engineer => {
     <div class="col-3"
         <div class="card" style="width: 18rem;">
         <div class="card-header">
-            {{ name }}
+            <h3>${engineer.name}</h3>
+            <h4>Engineer</h4>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: {{ id }}</li>
-            <li class="list-group-item">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/{{ github }}" target="_blank">{{ github }}</a></li>
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></li>
         </ul>
         </div>
     </div>
@@ -40,12 +42,13 @@ let generateInternCard = intern => {
     <div class="col-3"
         <div class="card" style="width: 18rem;">
         <div class="card-header">
-            {{ name }}
+            <h3>${intern.name}</h3>
+            <h4>Intern</h4>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: {{ id }}/li>
-            <li class="list-group-item">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
-            <li class="list-group-item">School: {{ school }}</li>
+            <li class="list-group-item">ID: ${intern.id}/li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+            <li class="list-group-item">School: ${intern.school}</li>
         </ul>
         </div>
     </div>
